98ef0ab1d7cc44bd23d236c475ec0633d2f50d16
Ido's VM <ido.ka@campus.technion.ac.il>
2022-05-14 08:21:47 +0000

    work from vm on cd

--- Commands.cpp
+++ Commands.cpp
@@ -105,9 +105,9 @@ void GetCurrDirCommand::execute(){
   cout << getcwd(path_buff, GetCurrDirCommand::MAX_PATH_LENGTH) << endl;
 }
 
-void JobsList::JobEntry::kill(){//TODO - REAL IMPLEMENT
+/*void JobsList::JobEntry::kill(){//TODO - REAL IMPLEMENT
   return;
-}
+}*/
 
 bool JobsList::JobEntry::stop(){
   if(status != Status::running || kill(pid, SIGTSTP) != 0){
