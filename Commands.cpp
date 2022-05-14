779b79bf2523e59b0ab4892dedcdb18581c4340d
IdoKarlinski <idokarli@gmail.com>
2022-05-14 14:12:08 +0300

    cd work

--- Commands.cpp
+++ Commands.cpp
@@ -347,11 +347,12 @@ void ChangeDirCommand::execute(){
             std::string old_path_trim = old_path.substr(0, idx);
             new_path = strcpy(new char [old_path_trim.length() + 1], old_path_trim.c_str());
             //new_path = old_path_trim.c_str();
-            path_stack.push_back(curr_path);
+            SmallShell::getInstance().old_path_stack.push_back(curr_path);
         }
     else {
         new_path = args[1];
-        path_stack.push_back(curr_path);
+        SmallShell::getInstance().old_path_stack.push_back(curr_path);
+        std::cout << SmallShell::getInstance().old_path_stack.size() << "\n";
         std::cout << curr_path << "\n";
     }
   //prepare();
