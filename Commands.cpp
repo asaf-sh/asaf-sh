09814ed5b2234aa84ce53371aed15667c3c162aa
IdoKarlinski <idokarli@gmail.com>
2022-05-15 20:46:32 +0300

    tail fix

--- Commands.cpp
+++ Commands.cpp
@@ -541,7 +541,7 @@ bool TailCommand::validate()
         std::cerr << "smash error: tail: invalid arguments \n";
         return false;
     }
-    if (isNumber(validate_if_num))
+    if (!isNumber(validate_if_num))
     {
         std::cout << 3 << "\n";
         std::cerr << "smash error: tail: invalid arguments \n";
